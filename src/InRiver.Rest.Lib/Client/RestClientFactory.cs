1537b963d581d286d4697bf45d3e2c0f2819e1a1
Adrian Janeczko <43387145+AdrianJaneczko@users.noreply.github.com>
2022-12-19 14:50:24 +0100

    When RestClient is created, it tries to set some properties on httpClient. There was an exception thrown when trying to modify properties of existing httpClient. (#5)
    Co-authored-by: Adrian Janeczko <Adrian.Janeczko@columbusglobal.com>

---
+++ src/InRiver.Rest.Lib/Client/RestClientFactory.cs
@@ -0,0 +1,42 @@
+﻿using RestSharp;
+using System.Net.Http;
+
+namespace InRiver.Rest.Lib.Client
+{
+    public interface IRestClientFactory
+    {
+        RestClient Create(RestClientOptions configuration);
+    }
+
+    public class RestClientFactory : IRestClientFactory
+    {
+        private static readonly object Lock = new object();
+
+        private readonly HttpClient _httpClientOverride;
+        private RestClient _restClient;
+
+        public RestClientFactory(HttpClient httpClientOverride = null)
+        {
+            _httpClientOverride = httpClientOverride;
+        }
+
+        public RestClient Create(RestClientOptions configuration)
+        {
+            if (_httpClientOverride == null)
+                return new RestClient(configuration);
+
+            if (_restClient == null)
+            {
+                lock (Lock)
+                {
+                    if (_restClient == null)
+                    {
+                        _restClient = new RestClient(_httpClientOverride, configuration);
+                    }
+                }
+            }
+
+            return _restClient;
+        }
+    }
+}
