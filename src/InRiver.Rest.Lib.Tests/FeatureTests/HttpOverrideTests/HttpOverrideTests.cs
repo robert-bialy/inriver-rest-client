faadef119886df8aab94fcc077aa524b79cc91ce
Robert Bialy <robert.bialy@columbusglobal.com>
2022-12-19 14:58:53 +0100

    feat: add a case for concurrent requests

--- src/InRiver.Rest.Lib.Tests/FeatureTests/HttpOverrideTests/HttpOverrideTests.cs
+++ src/InRiver.Rest.Lib.Tests/FeatureTests/HttpOverrideTests/HttpOverrideTests.cs
@@ -36,5 +36,35 @@ namespace InRiver.Rest.Lib.Tests.FeatureTests.HttpOverrideTests
             //Assert
             response.StatusCode.Should().Be((int)HttpStatusCode.Created);
         }
+
+        [Test]
+        public async Task HttpClientIsOverwritten_SendsConcurrentRequests()
+        {
+            //Arrange
+            const string host = "apieuw.productmarketingcloud.com";
+            const string apiKey = "11";
+            var options = new HttpClientInterceptorOptions();
+            var builder = new HttpRequestInterceptionBuilder();
+            builder
+                .Requests()
+                .ForPost()
+                .ForHttps()
+                .ForHost(host)
+                .ForPath("/api/v1.0.0/entities:createnew")
+                .Responds()
+                .WithStatus(HttpStatusCode.Created)
+                .RegisterWith(options);
+            using var httpClient = options.CreateHttpClient();
+            var client = new InRiverRestClient(apiKey, $"https://{host}", httpClient);
+
+            //Act
+            var request1 = client.EntityApi.CreateEntityAsyncWithHttpInfo(new EntityCreationModel("Item"));
+            var request2 = client.EntityApi.CreateEntityAsyncWithHttpInfo(new EntityCreationModel("Item"));
+            await Task.WhenAll(request1, request2);
+
+            //Assert
+            request1.Result.StatusCode.Should().Be((int)HttpStatusCode.Created);
+            request2.Result.StatusCode.Should().Be((int)HttpStatusCode.Created);
+        }
     }
 }
